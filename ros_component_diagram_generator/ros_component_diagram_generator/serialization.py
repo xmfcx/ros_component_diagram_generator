ca9fbf5554ab6bfbaafc52829b88392c7ae669d1
Leo Dive <project@leodrive.ai>
2023-09-25 15:39:41 +0300

    expanding on what is available

--- ros_component_diagram_generator/ros_component_diagram_generator/serialization.py
+++ ros_component_diagram_generator/ros_component_diagram_generator/serialization.py
@@ -37,12 +37,31 @@ def _make_entity_serializable(entity: launch.LaunchDescriptionEntity):
         d["name"] = entity.node_name
 
     if type(entity) is launch_ros.actions.Node:
-        d["package"] = entity.node_package
-        d["executable"] = entity.node_executable
-        d["namespace"] = entity._Node__expanded_node_namespace.replace(
-            launch_ros.actions.Node.UNSPECIFIED_NODE_NAMESPACE, "/"
-        )
-        d["name"] = entity._Node__expanded_node_name
+        assert isinstance(entity, launch_ros.actions.Node)
+        d["node_package"] = entity.node_package
+        d["node_executable"] = entity.node_executable
+        d["expanded_node_namespace"] = entity.expanded_node_namespace
+        d["name"] = entity.name
+        d["node_name"] = entity.node_name
+
+        try:
+            str_remapping_rules = str(set(entity.expanded_remapping_rules))
+        except:
+            str_remapping_rules = "None"
+
+        d["expanded_remapping_rules"] = str_remapping_rules
+        # print(entity.env)
+        # d["env"] = entity.env
+        # d["additional_env"] = entity.additional_env
+
+        str_command = str(entity.cmd)
+        # replace [] with {}
+        str_command = str_command.replace("[", "{")
+        str_command = str_command.replace("]", "}")
+
+        print(str_command)
+        d["cmd"] = "<back:white><font:Courier New>" + str_command + "</font></back>"
+        # d["cwd"] = entity.cwd
 
     if type(entity) is launch.actions.ExecuteProcess:
         d["name"] = entity.name
